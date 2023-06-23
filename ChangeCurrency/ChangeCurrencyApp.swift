e6694304d1fbb0d624198bb7fd9a2ca37d09569f
kepsonDiaz <kherabadiaby328@gmail.com>
2023-06-23 00:39:57 +0000

    Initial Commit

---
+++ ChangeCurrency/ChangeCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeCurrencyApp.swift
+//  ChangeCurrency
+//
+//  Created by Kheraba on 23/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChangeCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
